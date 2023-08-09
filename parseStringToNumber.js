8fd58771c81b88514e2f42ba452bcb53a9752d86
chethan5344 <57862717+chethan5344@users.noreply.github.com>
2023-08-09 18:24:55 +0530

    Create parseStringToNumber.js

---
+++ parseStringToNumber.js
@@ -0,0 +1,40 @@
+let err = 0
+
+function parser(str, radix = 10) {
+
+    if (!str.length) return
+
+    let a = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        const digit = convertCharToDigt(str[i]);
+
+        if (digit == 10) {
+            ++err
+            return
+        }
+
+        a *= radix;
+        a += digit;
+    }
+
+    return a
+}
+
+function convertCharToDigt(char) {
+    switch (true) {
+        case (char >= '0' && char <= '9'):
+            return char - '0';
+        default:
+            return 10;
+    }
+}
+
+const testData = ['123456', "1234567899", "asd1322", "!wqe", "qwe4566"]
+
+for (let index = 0; index < testData.length; index++) {
+    const d = parser(testData[index]);
+    if(d) console.log(typeof d, d);
+
+}
+console.log(`invalid string: ${err} of ${testData.length}`);
