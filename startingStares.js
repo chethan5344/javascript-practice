43775aee8ac1f62e260aa7200346949160fb3570
chethan5344 <57862717+chethan5344@users.noreply.github.com>
2023-08-09 18:34:51 +0530

    Update startingStares.js

--- startingStares.js
+++ startingStares.js
@@ -1,13 +1,14 @@
 function stepsStart() {
     const a = [5, -4, -2, 3, 1]
-    let x = 1
+    let x = 1, b = 1; // start point
     let i = 0
-    let b = 1
     while (i < a.length) {
+        // x is the number of steps climbed 
         x = (x + a[i])
         if (x >= 1) {
             i++
         } else {
+            // x is also incremented because change of start point
             x = b + 1 
             b = b + 1
             i = 0
