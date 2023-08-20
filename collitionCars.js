dac857db36abcf6bd8f1da5be5a70f55139f8310
chethan5344 <57862717+chethan5344@users.noreply.github.com>
2023-08-20 20:44:30 +0530

    Update collitionCars.js

--- collitionCars.js
+++ collitionCars.js
@@ -1,1 +1,29 @@
 
+var countCollisions = function (directions) {
+    let start, end, counter = 0, n = directions.length;
+
+    for (let i = 0; i < n; i++) {
+      if (directions[i] !== 'L') {
+        start = i;
+        break;
+      }
+    }
+      
+  
+    for (let i = n - 1; i >= 0; i--) {
+      if (directions[i] !== 'R') {
+        end = i;
+        break;
+      }
+    }
+      
+  
+    for (let i = start; i <= end; i++) {
+      if (directions[i] !== 'S') {
+        counter++;
+      }
+    }
+      
+  
+    return counter;
+};
